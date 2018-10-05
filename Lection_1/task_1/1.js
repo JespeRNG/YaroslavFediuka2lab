7d0c5732dc0f623579b1a101720dd522cccee340
JespeRNG <viacheslavovich25122001@gmail.com>
2018-10-05 22:30:58 +0300

    Added first task for first lection (schedule)

---
+++ Lection_1/task_1/1.js
@@ -0,0 +1,9 @@
+function shw_calls(){
+	document.getElementById('header').innerHTML="Schedule of calls";
+	document.getElementById('img').src="./calls.jpg";
+}
+
+function shw_lessons(){
+	document.getElementById('header').innerHTML="Schedule of lessons";
+	document.getElementById('img').src="./lessons.jpg";
+}
