f1911d09a586640f47e004e7a767e53ed30875b0
JespeRNG <viacheslavovich25122001@gmail.com>
2018-10-10 10:44:53 +0300

    Added a task about file service in node.js

---
+++ Lection_3/task_3/1.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+var fs = require("fs");
+
+app.get("/files", function(req, res){
+	myWebPage="<html><head><title><Files service></title><meta charset=\"UTF-8\"</head><body>";
+	myWebPage+="<table border=\"1\"><tr><td>Files You have: </td></tr>";
+	fs.readdir ("/var/www/html/NG_works/Data", function(err, data){
+		if (err){
+			console.log(err);
+		}else{
+			for(var i=0; i<data.length; i++){
+				file = data[i];
+				stats = fs.statSync("/var/www/html/NG_works/Data/" + file);
+				if (stats.isDirectory() == false){
+					myWebPage+="<tr><td><a href=\"http://localhost:9090/var/www/html/NG_works/Data"+ file + "\"download>"+ data[i] + "</a></td></tr>";
+				}
+			}
+		}
+		myWebPage += "</table></body></html>";
+		res.write(myWebPage);
+		res.end();
+	});
+	res.write;
+});
+
+app.listen(9090, function(err){
+	if (err){
+		console.log(err);
+	}else{
+		console.log("Everything seems to be working :");
+	}
+});
