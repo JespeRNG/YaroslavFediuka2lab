6d4e4e48f694066644834c8d66fba5e64b4b3c4c
JespeRNG <viacheslavovich25122001@gmail.com>
2018-10-11 18:33:05 +0300

    Updated task about Caesar cipher

--- Lection_2/task_1/T1.js
+++ Lection_2/task_1/T1.js
@@ -1,28 +1,33 @@
- function cipher(){
- 	var shift = +document.getElementById('shift').value;
+ function toEncrypt(){
+	var shift = document.getElementById('shift').value;
+	shift = (26 + shift) % 26;
 	var str = document.getElementById('input').value;
-	var out = '';
-    
-        
-	for (var i=0; i< str.length; i++){
-      
-		  var code = str.charCodeAt(i);
-		  code = code + shift;
-		  out += String.fromCharCode(code);
-    }
-	alert(out);
+	var result = "";
+
+	for (var i = 0; i < str.length; i++) {
+		var code = str.charCodeAt(i);
+
+		if(65 <= code && code <=  90) result += String.fromCharCode((code - 65 + shift) % 26 + 65);  // Uppercase
+		else if(97 <= code && code <= 122) result += String.fromCharCode((code - 97 + shift) % 26 + 97);  // Lowercase
+		else result += str.charAt(i);  // Copy
+	}
+	alert (result);
+	return result;
 }
 
-function uncipher(){
-	var shift = -document.getElementById('shift').value;
+function toDecrypt() {
+	var shift = document.getElementById('shift').value;
+	shift = (26+shift) % 26;
 	var str = document.getElementById('input').value;
-	var out = '';
-    
-       
-	for (var i=0; i< str.length; i++){
+	var result = '';
+
+	for (var i=0; i < str.length; i++) {
 		var code = str.charCodeAt(i);
-		code = code + shift;
-		out += String.fromCharCode(code);
+
+		if (65 <= code  && code <= 90) result += String.fromCharCode((code - 65 - shift) % 26 +65);
+		else if (97 <= code && code <= 122) result += String.fromCharCode((code - 97 - shift) % 25 + 97);
+		else result += str.charAt(i);
 	}
-	alert(out);
-}
+	alert(result);
+	return result;
+};
