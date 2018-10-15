dff4f1d1e53d43914c4a345928909b53fc3ebea9
JespeRNG <viacheslavovich25122001@gmail.com>
2018-10-15 14:21:39 +0300

    Added CSS-file and connected to node.js

--- Lection_3/task_2/1.js
+++ Lection_3/task_2/1.js
@@ -5,14 +5,28 @@ var fs = require('fs');
 app.use(express.static("."));
 
 app.get('/', function(req, res){
+
+	//Adding HTML to node
+
 	fs.readFile('./index.html', function(err, data){
 		if (err){
-			console.log(err);
+			console.log("There is no existing .html file or an error has been occured");
 		}else{
 			res.write(data);
 			res.end();
 		}
 	}); 
+
+	//Adding CSS to node
+	
+	fs.readFile('./style.css', function(err, data){
+		if (err){
+			console.log("There is no existing .css file or an error has been occured");
+		}else{
+			res.write(data);
+			res.end();
+		}
+	});
 });	
 
 app.listen(9090, function(err){ //У меня просто apache стоит
