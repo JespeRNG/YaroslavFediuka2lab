299a20a43b9c04ef954ca89cf9678374046de92a
JespeRNG <viacheslavovich25122001@gmail.com>
2018-10-15 13:52:03 +0300

    Updated Caesar cipher

--- Lection_2/task_1/T1.js
+++ Lection_2/task_1/T1.js
@@ -2,6 +2,7 @@
 	var shift = document.getElementById('shift').value;
 	shift = (26 + shift) % 26;
 	var str = document.getElementById('input').value;
+	var res = document.getElementById("res");
 	var result = "";
 
 	for (var i = 0; i < str.length; i++) {
@@ -9,9 +10,9 @@
 
 		if(65 <= code && code <=  90) result += String.fromCharCode((code - 65 + shift) % 26 + 65);  // Uppercase
 		else if(97 <= code && code <= 122) result += String.fromCharCode((code - 97 + shift) % 26 + 97);  // Lowercase
-		else result += str.charAt(i);  // Copy
+		else result += str.charAt(i);
 	}
-	alert (result);
+	res.innerHTML = result;
 	return result;
 }
 
@@ -19,7 +20,8 @@ function toDecrypt() {
 	var shift = document.getElementById('shift').value;
 	shift = (26+shift) % 26;
 	var str = document.getElementById('input').value;
-	var result = '';
+	var res = document.getElementById("res");
+	var result = "";
 
 	for (var i=0; i < str.length; i++) {
 		var code = str.charCodeAt(i);
@@ -28,6 +30,6 @@ function toDecrypt() {
 		else if (97 <= code && code <= 122) result += String.fromCharCode((code - 97 - shift) % 25 + 97);
 		else result += str.charAt(i);
 	}
-	alert(result);
+	res.innerHTML = result;
 	return result;
 };
